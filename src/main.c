e77df97c8ac50015ee2fbfffb433d6659fab2773
pnaessen <pnaessen@student.42lyon.fr>
2026-02-28 10:05:12 +0100

    implement ft_strlen and ft_strncmp functions in main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
 #include "hotrace.h"
 
-
-int main(int argc, char const **argv)
+int ft_strlen(char *str)
 {
-	return 0;
+    int i;
+
+    i = 0;
+    while (str[i]) {
+	i++;
+    }
+    return (i);
 }
 
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+    unsigned char *t1;
+    unsigned char *t2;
+
+    t1 = (unsigned char *)s1;
+    t2 = (unsigned char *)s2;
+    i = 0;
+    while (i < n) {
+	if (t1[i] != t2[i])
+	    return (t1[i] - t2[i]);
+	if (t1[i] == '\0')
+	    return (0);
+	i++;
+    }
+    return (0);
+}
+
+int main(int argc, char const **argv) { return 0; }
